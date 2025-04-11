d26a35349f496546694e3b4994897fe54c42ab38
geovana-09 <rm95029@estudante.fieb.edu.br>
2025-04-11 10:39:57 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
 import { useState } from "react"
 
- function App(){
- 
-  const[nome, setNome] = useState('Geovana')
+function App(){
 
-  const Formulario = () => {
+  const [nome, setNome] = useState('Ronaldo')
 
-   
-      
-<> 
-<h3>Formulário</h3>
-<div>
-<h3> Pizzaria 2D</h3>
-<input
-className="nome"
-onChange ={(e)=>{setNome (e.target.value)}}
-type="text" />
- 
-  <button
-  className="botao"
-onClick={()=>{
-  alert('O nome digitado foi' + nome )
-    }
-  }
->
-  CLIQUE AQUI
-  <div>
- </button>
- <h3>Pizzaria 2D</h3>
- <p>texto do paragrafo</p>
-<img src={} />
- <Formulario />
- <Formulario />
- <Formulario />
- <Formulario />
+  return (
+    <div>
+      <h3>Pizzaria 2D</h3>
 
-   </div>
- ) 
+      <input
+        className="nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        type="text" />
+
+      <button
+        className="botao"
+        onClick={()=>{
+          alert('O nome digita foi ' + nome)
+        }
+        }
+      >
+        CLIQUE AQUI
+      </button>
+
+    </div>
+  )
 }
 
 export default App
+ 
